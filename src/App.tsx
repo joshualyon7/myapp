247a7bb48924461ffbb4bda7e17f9c4cf4732cde
Josh Lyon <joshlyon@udel.edu>
2021-09-25 14:42:33 -0400

    Fixed typescript being mad

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <button onClick={() => setCount(count + 1)}>click me!</button>
         <Jumbotron>{count}</Jumbotron>
-        <button onClick={() => setCount(count + 1)}>Click me!</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
